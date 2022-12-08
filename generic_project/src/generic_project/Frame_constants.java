7081dbd6803af7a92ef8fce2d68ebafec7b165fa
Rohit <Rohit@Rohit>
2022-12-08 10:54:51 +0530

    anything

---
+++ generic_project/src/generic_project/Frame_constants.java
@@ -0,0 +1,14 @@
+package generic_project;
+
+public interface Frame_constants 
+{
+
+	String gecko_key="webdriver.gecko.driver";
+	String gecko_value="./software/geckodriver.exe";
+	
+	String chrome_key="webdriver.chrome.driver";
+	String chrome_value="./software/chromedriver.exe";
+	
+	String url="https://www.facebook.com";
+	
+}
